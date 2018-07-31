2d0113fe1a3ad59b2d641323e49f5c5cb65341f8
meoncu <meoncu@gmail.com>
2018-07-31 15:26:14 +0300

    Update Fragment_Senet.java

--- app/src/main/java/tr/org/tkk/tkk_ortak/Fragment_Senet.java
+++ app/src/main/java/tr/org/tkk/tkk_ortak/Fragment_Senet.java
@@ -27,7 +27,7 @@ public class Fragment_Senet extends Fragment {
     FragmentActivity listener;
     private ProgressDialog pDialog;
     private ListView lv;
-    private static String url = "https://ekoop.tarimkredi.org.tr/rest/ortakTakip/ortakSenetDurumlari?ortak_no=1898&kurum_no=%27K070845%27";
+    private static String url = "";
     ArrayList<HashMap<String, String>> contactList;
 
     @Nullable
